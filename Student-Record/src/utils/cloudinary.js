7509433481772b6651a9abde08e87c9cccf4016f
Mohammad Tasin <mohammadtasinspj@gmail.com>
2025-07-16 22:33:20 +0530

    feat: enhance cloudinary configuration and uploader error handling

--- Student-Record/src/utils/cloudinary.js
+++ Student-Record/src/utils/cloudinary.js
@@ -1,7 +1,26 @@
 import { v2 as cloudinary } from "cloudinary";
 
+cloudinary.config({
+  cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+  api_key: process.env.CLOUDINARY_API_KEY,
+  api_secret: process.env.CLOUDINARY_API_SECRET,
+});
+
 const fileUploder = async (filePath) => {
   try {
     if (!filePath) return null;
-  } catch (error) {}
+
+    const response = await cloudinary.uploader.upload(filePath, {
+      public_id: "avatars",
+      resource_type: "auto",
+    });
+
+    console.log(`Response :- ${response}`);
+    return response;
+  } catch (error) {
+    console.error("Error for uploding on cloudinary :- ", error);
+    return null;
+  }
 };
+
+export default fileUploder;
